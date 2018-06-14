1965161d9ca8efffd83cb2d0d5eb5b8575d8ef68
Lorenzo Turrino <lorenzo.turrino@bbc.co.uk>
2018-06-14 18:13:43 +0100

    add requirements and python version, pep8

--- playlist_creator.py
+++ playlist_creator.py
@@ -5,16 +5,18 @@ from base64 import b64encode
 def read_credentials(file_name='credentials.txt'):
     with open(file_name, 'r') as f:
         return f.read().splitlines()[:2]
-        
+
+
 def get_server_token():
     auth_endpoint = 'https://accounts.spotify.com/api/token'
     client_id, secret = read_credentials()
     auth_token = b64encode(f'{client_id}:{secret}'.encode('utf-8')).decode()
     return requests.post(
-        auth_endpoint, 
-        data={'grant_type': 'client_credentials'}, 
+        auth_endpoint,
+        data={'grant_type': 'client_credentials'},
         headers={'Authorization': f'Basic {auth_token}'}
     )
+
 
 def search_artist_id(name, auth_token):
     search_endpoint = 'https://api.spotify.com/v1/search'
@@ -25,6 +27,7 @@ def search_artist_id(name, auth_token):
     )
     artist = r.json()['artists']['items'][0]
     return artist['id'], artist['name']
+
 
 # sample queries
 auth_token = get_server_token().json()['access_token']
