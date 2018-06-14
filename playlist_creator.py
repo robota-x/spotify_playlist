354f18618b0390c25290a727cb0c3bffd75cfe53
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-06-14 08:48:33 +0100

    add artist id search by name

--- playlist_creator.py
+++ playlist_creator.py
@@ -10,5 +10,23 @@ def get_server_token():
     auth_endpoint = 'https://accounts.spotify.com/api/token'
     client_id, secret = read_credentials()
     auth_token = b64encode(f'{client_id}:{secret}'.encode('utf-8')).decode()
-    return requests.post(auth_endpoint, data={'grant_type': 'client_credentials'}, headers={'Authorization': f'Basic {auth_token}'})
+    return requests.post(
+        auth_endpoint, 
+        data={'grant_type': 'client_credentials'}, 
+        headers={'Authorization': f'Basic {auth_token}'}
+    )
 
+def search_artist_id(name, auth_token):
+    search_endpoint = 'https://api.spotify.com/v1/search'
+    r = requests.get(
+        search_endpoint,
+        params={'q': name, 'type': 'artist', 'limit': 1},
+        headers={'Authorization': f'Bearer {auth_token}'}
+    )
+    artist = r.json()['artists']['items'][0]
+    return artist['id'], artist['name']
+
+# sample queries
+auth_token = get_server_token().json()['access_token']
+sample_query = search_artist_id('Lady Gaga', auth_token)
+print(sample_query)
