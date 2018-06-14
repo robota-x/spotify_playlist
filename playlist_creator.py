4fc870867140aed5a70abba62961b96ebaaa9c69
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-06-14 08:31:41 +0100

    add authorisation flow for server

--- playlist_creator.py
+++ playlist_creator.py
@@ -1,10 +1,14 @@
+import requests
+from base64 import b64encode
+
+
 def read_credentials(file_name='credentials.txt'):
     with open(file_name, 'r') as f:
-        client_id, secret = f.read().splitlines()[:2]
-        return {
-            'client_id': client_id,
-            'secret': secret
-        }
+        return f.read().splitlines()[:2]
         
+def get_server_token():
+    auth_endpoint = 'https://accounts.spotify.com/api/token'
+    client_id, secret = read_credentials()
+    auth_token = b64encode(f'{client_id}:{secret}'.encode('utf-8')).decode()
+    return requests.post(auth_endpoint, data={'grant_type': 'client_credentials'}, headers={'Authorization': f'Basic {auth_token}'})
 
-print(read_credentials())
