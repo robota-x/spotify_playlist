fa6b3390df5fb4f9657884e4bc370dc6d78bf2aa
Lorenzo Turrino <lorenzo.turrino@bbc.co.uk>
2018-06-14 18:47:38 +0100

    checkpoint api_request wrapper

--- playlist_creator.py
+++ playlist_creator.py
@@ -8,46 +8,61 @@ class SpotifyClient:
     TOP_TRACKS_ENDPOINT = 'https://api.spotify.com/v1/artists/{id}/top-tracks'
 
     def __init__(self, file_name='credentials.txt'):
-        self.client_id, self.secret = self.read_credentials(file_name)
+        self.client_id, self.secret = self._read_credentials(file_name)
+        self._update_server_token()
 
-    def read_credentials(self, file_name):
+    def _read_credentials(self, file_name):
         with open(file_name, 'r') as f:
             return f.read().splitlines()[:2]
 
-    def update_server_token(self):
+    def _api_request(self, endpoint, method='get', headers=None, data=None, params=None):
+        r = getattr(requests, method)(
+            endpoint,
+            params=params,
+            data=data,
+            headers=headers or {'Authorization': f'Bearer {self.auth_token}'}
+        )
+        r.raise_for_status()
+        return r.json()
+
+    def _update_server_token(self):
         auth_string = b64encode(
             f'{self.client_id}:{self.secret}'.encode('utf-8')).decode()
-        self.auth_token = requests.post(
+
+        self.auth_token = self._api_request(
             self.AUTH_ENDPOINT,
             data={'grant_type': 'client_credentials'},
-            headers={'Authorization': f'Basic {auth_string}'}
-        ).json()['access_token']
+            headers={'Authorization': f'Basic {auth_string}'},
+            method='post'
+        )['access_token']
 
-    def get_artist_by_name(self, name):
-        r = requests.get(
+
+    def get_artist_by_name(self, name, strict=True):
+        res = requests.get(
             self.SEARCH_ENDPOINT,
             params={'q': name, 'type': 'artist', 'limit': 1},
             headers={'Authorization': f'Bearer {self.auth_token}'}
-        )
-        artist = r.json()['artists']['items'][0]
-        return artist['id'], artist['name']
+        ).json()
+
+        if len(res):
+            artist = res['artists']['items'][0]
+            return name.lower() == artist['name'].lower() and artist['id'] if strict else artist['id']
 
     def get_top_songs_by_artist_id(self, artist_id, country='GB'):
-        r = requests.get(
+        res = requests.get(
             self.TOP_TRACKS_ENDPOINT.format(id=artist_id),
             params={'country': country},
             headers={'Authorization': f'Bearer {self.auth_token}'}
-        )
-        track_blob = r.json()['tracks']
+        ).json()
+
+        track_blob = res['tracks']
         return [{'id': track['id'], 'name': track['name']} for track in track_blob]
 
 
 # sample queries
 client = SpotifyClient()
 
-client.update_server_token()
-
-artist_id = client.get_artist_by_name('Lady Gaga')[0]
+artist_id = client.get_artist_by_name('Lady gaga')
 top_songs = client.get_top_songs_by_artist_id(artist_id)
 
 print(top_songs)
